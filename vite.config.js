18bf6544b8cec9abf4e35b06605eb025fdf9f869
GitOps <nadilfernando@Mac.powerhub>
2026-04-14 11:35:31 +0100

    change Domain

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-  },
   preview: {
     host: '0.0.0.0',
     port: 4173,
-  },
+    allowedHosts: [
+      'setupdesk.co.uk',
+      'www.setupdesk.co.uk'
+    ]
+  }
 })
